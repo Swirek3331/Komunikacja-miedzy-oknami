2411b9f1f3bc54dc64141af2b7fc8d12c29255f0
Swirek3331 <karolsutkowski21@gmail.com>
2023-04-23 13:06:02 +0100

    Struktura

--- src/forms/Oct.cs
+++ src/forms/Oct.cs
@@ -4,16 +4,15 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace KarolSutkowski1TPKomunikacjaAplikacjiMDI
+namespace KarolSutkowski1TPKomunikacjaAplikacjiMDI.src.forms
 {
-    public partial class Form1 : Form
+    public partial class Oct : Form
     {
-        public Form1()
+        public Oct()
         {
             InitializeComponent();
         }
